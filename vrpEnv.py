ab0f15cf3501c85e0fc233e7f78eedb40792b0fd
Jondiii <jondi@opendeusto.es>
2023-02-16 10:27:19 +0100

    métodos básicos funcionales

--- vrpEnv.py
+++ vrpEnv.py
@@ -6,23 +6,28 @@ import gym
 from gym import spaces
 import numpy as np
 
-from rutas import Rutas
+verbose = False
 
 class VRPEnv(gym.Env):
     metadata = {'render.modes': ['human']}
 
     def __init__(self, nVehiculos = 1, nNodos = 20, nGrafos = 10, nVisualizaciones = 5):
         self.step_count = 0
-        self.nNodos = nNodos
+        self.nNodos = nNodos + 1 # +1 del depot
         self.nGrafos = nGrafos
         self.nVehiculos = nVehiculos
         self.nVisualizaciones = nVisualizaciones
 
         # Tantas acciones como (número de nodos + depot) * número de vehículos
-        nActions = (nNodos + 1) * nVehiculos
+        self.nActions = self.nNodos * self.nVehiculos
 
-        self.action_space = spaces.Discrete(nActions)
-        self.actions = np.arrange(stop = nActions)
+        print("nActions {}".format(self.nActions)) if verbose else None
+        self.action_space = spaces.Discrete(self.nActions)
+
+        self.observation_space = spaces.Dict({
+            "visited" :  spaces.MultiDiscrete(np.zeros(shape=(self.nVehiculos, self.nNodos)) + 2),
+            "curr_position" : spaces.MultiDiscrete(np.zeros(shape=self.nVehiculos) + self.nNodos)
+        })
 
     def step(self, action):
         self.step_count += 1
@@ -33,22 +38,31 @@ class VRPEnv(gym.Env):
                 self.getState(),
                 -1,  # reward
                 False,
-                None
+                dict()
             )
         
-        # supongamos que nNodos = 6, nVehiculos = 2 y action = 14
+        # supongamos que nNodos = 6, nVehiculos = 2 y action = 6 * 2 + 2
+        # Calculamos el vehículo que realiza la acción
 
-        vehiculo = floor(action % self.nNodos) # Calculamos el vehículo que realiza la acción
+        vehiculo = action // self.nNodos
         # vehiculo = 1, es decir, el segundo vehículo
-
-        action -= (self.nNodos + 1) * vehiculo
-        # action = 14 - (6 + 1) * 1 = 7
+        print("action antes {}".format(action)) if verbose else None
         
+        action = action % self.nNodos
+        # action = 14 % 6 = 2 # Sería visitar el tercer nodo
+        
+        if verbose:
+            print("nNodos {}".format(self.nNodos))
+            print("action despues {}".format(action))
+            print("vehiculo {}".format(vehiculo))
+
         # Eliminar el lugar que se acaba de visitar de las posibles acciones
         self.visited[:,action] = 1
+        print(self.visited)  if verbose else None
 
         # Variar posición del vehículo que realice la acción
         self.posicionActual[vehiculo] = action
+        print("posicionActual {}".format(self.posicionActual)) if verbose else None
 
         # Calcular la recompensa #TODO
         reward = 1
@@ -60,29 +74,32 @@ class VRPEnv(gym.Env):
             self.getState(),
             reward,
             done,
-            None
+            dict()
         )
+
 
     def reset(self):
         self.step_count = 0
-
         self.visited = np.zeros(shape=(self.nVehiculos, self.nNodos))
-
-        self.posicionActual = np.zeros(self.nVehiculos)
+        self.posicionActual = np.zeros(shape = self.nVehiculos)
 
         return self.getState()
 
-    def checkAction(self, actions) -> bool:
-        pass
+    def checkAction(self, action) -> bool:
+        return True
     
     def getState(self):
-        pass
+        obs = dict()
+        obs["visited"] = self.visited
+        obs["curr_position"] = self.posicionActual
+
+        return obs
 
     def generate_mask(self):
         pass
 
     def is_done(self):
-        return np.all(self.visited == 1)
+        return bool(np.all(self.visited == 1))
     
     def render(self):
         pass

93e20383e96de23fb6ef5f39796fa85e27d89895
Jondiii <jon.diaz@deusto.es>
2023-04-04 10:51:25 +0200

    comentario de prueba

--- envChecker.py
+++ envChecker.py
@@ -5,5 +5,7 @@ from stable_baselines3.common.env_checker import check_env
 env = VRPEnv()
 env.createEnv(50, 100, sameMaxNodeVehicles=True)
 
+# COMENTARIO DE PRUEBA
+
 # It will check your custom environment and output additional warnings if needed
 check_env(env)

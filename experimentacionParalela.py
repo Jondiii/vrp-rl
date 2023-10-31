fcbc97737b69c9ead85fdc55886fb58125db5702
Jondiii <jondi@opendeusto.es>
2023-10-31 10:14:43 +0100

    otra prueba

--- experimentacionParalela.py
+++ experimentacionParalela.py
@@ -102,7 +102,7 @@ def lanzarExperimento(nombreExp):
 
 if __name__ == '__main__':
     pool = multiprocessing.Pool(processes = 4)
-    result = pool.imap(prueba, listaPruebas)
+    result = pool.imap(lanzarExperimento, listaPruebas)
     #result = pool.map_async(lanzarExperimento, listaPruebas)
     #result.get()
     pool.close()

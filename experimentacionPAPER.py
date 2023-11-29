97cd90a2afeb384acf1fb1118131195963d62104
Jondiii <jondi@opendeusto.es>
2023-11-29 10:37:25 +0100

    experimentos 2

--- experimentacionPAPER.py
+++ experimentacionPAPER.py
@@ -1,4 +1,6 @@
+from stable_baselines3.common.results_plotter import load_results, ts2xy
 from stable_baselines3.common.callbacks import BaseCallback
+from stable_baselines3.common.monitor import Monitor
 from stable_baselines3 import PPO, DQN, A2C
 from vrpEnv import VRPEnv
 import os
@@ -8,7 +10,7 @@ import numpy as np
 ITERATIONS = 5
 TIMESTEPS = 2048*100 # Serán 5M de steps
 
-listaMetodo = ["normal", "increasing", "decreasing"]
+listaMetodo = ["normal", "increasing"]
 listaAlgoritmo = ["PPO", "DQN", "A2C"]
 #listaTamanyo = ["P", "M", "G"]
 #listaExpNumber = [*range(20)]
@@ -20,10 +22,43 @@ class CustomCallback(BaseCallback):
     shortestRoute = 100000000000000
     shortestRouteNodesVisited = None
 
-    def __init__(self, verbose=0):
+    def __init__(self, check_freq, log_dir, verbose=0):
             super().__init__(verbose)
+            self.check_freq = check_freq
+            self.log_dir = log_dir
+            self.save_path = os.path.join(log_dir, "best_model")
+            self.best_mean_reward = -np.inf
+
+    def _init_callback(self) -> None:
+        # Create folder if needed
+        if self.save_path is not None:
+            os.makedirs(self.save_path, exist_ok=True)
 
     def _on_step(self) -> bool:
+        if self.n_calls % self.check_freq == 0:
+
+            # Retrieve training reward
+            x, y = ts2xy(load_results(self.log_dir), "timesteps")
+            if len(x) > 0:
+                # Mean training reward over the last 100 episodes
+                mean_reward = np.mean(y[-100:])
+                if self.verbose > 0:
+                    print("Num timesteps: {}".format(self.num_timesteps))
+                    print(
+                        "Best mean reward: {:.2f} - Last mean reward per episode: {:.2f}".format(
+                            self.best_mean_reward, mean_reward
+                        )
+                    )
+
+                # New best model, you could save the agent here
+                if mean_reward > self.best_mean_reward:
+                    self.best_mean_reward = mean_reward
+                    # Example for saving best model
+                    if self.verbose > 0:
+                        print("Saving new best model at {} timesteps".format(x[-1]))
+                        print("Saving new best model to {}.zip".format(self.save_path))
+                    self.model.save(self.save_path)
+
         return True
 
     def _on_rollout_end(self) -> None:
@@ -34,15 +69,12 @@ class CustomCallback(BaseCallback):
         for tiempo in tiempos[0]:
                 tiempoTotal += tiempo
 
-        print(tiempoTotal)
-        print(self.shortestRoute)
-
         if tiempoTotal < self.shortestRoute:
             self.shortestRoute = tiempoTotal
 
             self.shortestRouteNodesVisited = np.count_nonzero(env.get_attr("visited")[:env.get_attr("nNodos")[0]] == 1) / env.get_attr("nNodos")[0]
 
-            with open("resultsPaper/"+env.get_attr("name")[0]+".txt", 'w', encoding="utf-8") as f:
+            with open("resultsPaper2/"+env.get_attr("name")[0]+".txt", 'w', encoding="utf-8") as f:
                 f.write(env.get_attr("name")[0])
                 f.write("\n")
                 f.write(str(self.shortestRoute))
@@ -76,7 +108,7 @@ def crearModelo(algoritmo, env, log_dir):
 
 
 def crearEnv(nVehiculos, nNodos, metodo, name):
-    env = VRPEnv(multiTrip = True, name = name) 
+    env = VRPEnv(multiTrip = False, name = name) 
     env.createEnv(nVehiculos = nVehiculos, nNodos = nNodos, maxNodeCapacity = 4, sameMaxNodeVehicles=True)
 
     if metodo == "decreasing":
@@ -94,15 +126,16 @@ for algoritmo in listaAlgoritmo:
     for metodo in listaMetodo:
         nombreExp = algoritmo + "_" + metodo
 
-        models_dir = "modelsPaper/" + nombreExp
-        log_dir = "logsPaper"
-        result_dir = "resultsPaper"
+        models_dir = "modelsPaper2/" + nombreExp
+        log_dir = "logsPaper2"
+        result_dir = "resultsPaper2"
 
-        callback = CustomCallback()
+        callback = CustomCallback(500, log_dir)
 
         crearDirectorios(models_dir, log_dir, result_dir)
 
         env = crearEnv(nVehiculos, nNodos, metodo, name =  nombreExp)
+        env = Monitor(env, log_dir)
 
         model = crearModelo(algoritmo, env, log_dir)
 
@@ -115,7 +148,7 @@ for algoritmo in listaAlgoritmo:
 
         print("---%s: %s minutos ---" % (nombreExp, round((time.time() - start_time)/60, 2)))
 
-        env.render("resultsPaper/"+nombreExp )
+        env.render("resultsPaper2/"+nombreExp )
         env.close()
 
 
